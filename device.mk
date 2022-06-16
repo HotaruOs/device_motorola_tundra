1160ed43e55dfeac36af747bd08cd6d1c5cae223
Cyber Knight <cyberknight755@gmail.com>
2022-06-17 02:12:27 +0800

    tundra: Build KProfiles
    - Inherit KProfiles.
    - Address sepolicy.
    - Define sysfs_kprofiles as a domain.
    - Fix permission of nodes relevant to KProfiles at boot.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- device.mk
+++ device.mk
@@ -379,6 +379,10 @@ PRODUCT_COPY_FILES += \
     system/core/libprocessgroup/profiles/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json \
     system/core/libprocessgroup/profiles/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json
     
+# KProfiles
+PRODUCT_PACKAGES += \
+    KProfiles
+    
 # Private keys
 #-include vendor/evolution-priv/keys/keys.mk
 
