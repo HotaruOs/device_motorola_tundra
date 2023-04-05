3a9d0d1ec5d0abc6eabc4c66df5372421c6c9154
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-04-05 17:26:29 +0530

    tundra: Add support for high brightness mode (HBM)
    Brightness from 3514 (500 nits) to 4095 (1100 nits) is HBM.
    Enter HBM at >=6500 lux (outdoor daylight) and hit peak brightness
    at >=10000 lux (under direct sunlight). Adjust the backlight curve
    accordingly.
    
    Requires increasing max brightness to 4095 in kernel.
    
    Change-Id: I069bade84942c4be2e2e33a9b7ef76f56c9e47a3

--- device.mk
+++ device.mk
@@ -125,6 +125,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
     $(LOCAL_PATH)/audio/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml
 
+# Display
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/display_id_4630947043778501762.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630947043778501762.xml
+
 # Boot control
 PRODUCT_PACKAGES += \
     android.hardware.boot-service.qti \
