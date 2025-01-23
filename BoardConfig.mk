975466d78897dd6412764f97318f94abc8f77dcc
HotaruOs <arroztlg@gmail.com>
2025-01-23 00:59:19 -0300

    tundra: use power-libperfmgr powerhint from https://github.com/PixelOS-Devices/device_oneplus_sm8350-common/blob/fourteen

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -154,6 +154,7 @@ TARGET_RECOVERY_UI_MARGIN_HEIGHT := 90
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Sepolicy
+include device/lineage/sepolicy/libperfmgr/sepolicy.mk
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
 BUILD_BROKEN_VENDOR_PROPERTY_NAMESPACE := true
