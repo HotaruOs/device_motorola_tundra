e6947fadbd5c123cfa5881efb567ca378725fca8
Marc Bourgoin <themard85@gmail.com>
2025-09-09 22:11:11 -0600

    tundra: Drop NFC 1.2 HIDL manifest entry
    Our devices are switching to AIDL NFC
    
    Change-Id: Iff8e226df0f5aed24bb9adfff8136408481820d8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -113,9 +113,6 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/hidl/manifest.xml
-ifeq ($(TARGET_HAS_SNXXX_NFC),)
-DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/hidl/manifest_nfc.xml
-endif
 
 # Metadata
 BOARD_USES_METADATA_PARTITION := true
