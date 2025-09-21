d11e86bc9af81ad3d77b32b41c426f27d1d32495
Dan Morrill <morrildl@google.com>
2025-09-22 01:03:44 +0200

    tundra: Adding a feature to indicate that the device possesses low-latency audio
    * The specific criteria for low-latency audio are defined in the CDD, but since
    that hasn't yet been settled for Gingerbread, this change doesn't go into
    details. We can do a docs change later once GB CDD is final.
    
    Change-Id: Ic5ad9d04e8ecd78c81b6f09439affec6bf8cb9b2
    Signed-off-by: HotaruOs <arroztlg@gmail.com>

--- device.mk
+++ device.mk
@@ -392,6 +392,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.hcef.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hcef.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
 
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml
+    
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti
